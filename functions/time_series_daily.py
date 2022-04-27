e9a8199186ef6e799fe3b81e53a2bc295d810a7c
davion <dchai@moneylion.com>
2022-04-27 23:54:05 +0800

    Add runnable barebone

---
+++ functions/time_series_daily.py
@@ -0,0 +1,16 @@
+from utils.strenum import StrEnum
+
+
+class OutputSize(StrEnum):
+    FULL = "full"  # Returns full length data
+    COMPACT = "compact"  # Returns latest 100 entries
+
+
+class DataType(StrEnum):
+    JSON = "json"
+    CSV = "csv"
+
+
+class AlphaVantageFunction:
+    def __init__(self, **kwargs) -> None:
+        self.params: dict = {"outputsize": OutputSize.FULL, "datatype": DataType.JSON}
---
+++ utils/request.py
@@ -0,0 +1,37 @@
+import logging
+import json
+import requests
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+ROOT_URL = "https://www.alphavantage.co/query"
+
+
+class AlphaVantageQuery:
+    def __init__(self, api_key: str, alpha_vantage_function: str):
+        self.api_key = api_key
+        self.alpha_vantage_function = alpha_vantage_function.upper()
+
+    def get_request(self, params: dict) -> requests.models.Response:
+        params.update({"apikey": self.api_key, "function": self.alpha_vantage_function})
+        try:
+            response: requests.models.Response = requests.get(ROOT_URL, params=params)
+        except requests.Timeout as error_timeout:
+            logger.error(f"{error_timeout}")
+        except requests.HTTPError as error_http:
+            logger.error(f"{error_http}")
+        except requests.ConnectionError as error_connection:
+            logger.error(f"{error_connection}")
+        except requests.RequestException as error_request:
+            logger.error(f"{error_request}")
+        except Exception as error_unkown:
+            logger.error(f"Unkown Error - {error_unkown}")
+        else:
+            if not response.ok:
+                logger.error(f"Response Headers - {response.headers}")
+                logger.error(f"Status Code - {response.status_code}")
+                logger.error(f"Response - {response.json()}")
+                raise ConnectionError("Exiting due to bad connection.")
+            else:
+                logger.info(f"Returning response from [{response.url}]")
+                return response
